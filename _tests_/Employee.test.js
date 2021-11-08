d117d17c459695291ed243b41074dd4e9f2512a2
Katlyn Boches <katlynboches@gmail.com>
2021-11-07 23:58:53 -0700

    updated functions

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,39 @@
+// Using Employee constructor
+const Employee = require('../lib/Employee');
+
+// Create employee object
+test('creates an employee object', () => {
+    const employee = new Employee('Nicole', 90, 'nicole.elisaw@gmail.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
+
+// Tests Id from getId()
+test('gets employee name', () => {
+    const employee = new Employee('', 90, 'nicole.elisaw@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+// gets id from getId() 
+test('gets employee ID', () => {
+    const employee = new Employee('Katlyn', 90, 'katlynboches@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+// gets emails from getEmail()
+test('gets employee email', () => {
+    const employee = new Employee('Katlyn', 90, 'katlynboches@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+// gets role from getRole()
+test('gets role of employee', () => {
+    const employee = new Employee('Katlyn', 90, 'katlynboches@gmail.com');
+
+    expect(employee.getRole()).toEqual("Employee");
+}); 
--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -4,13 +4,13 @@ function generateHTML(data) {
       <div class="col-4 mt-4">
           <div class="card h-100">
               <div class="card-header">
-                  <h3>${data.name}</h3>
+                  <h3>${manager.name}</h3>
                   <h4>Manager</h4><i class="material-icons">content_paste</i>
               </div>
               <div class="card-body">
-                  <p class="id">ID: ${data.id}</p>
-                  <p class="email">Email: <a href="mailto:${data.email}">${data.email}</a></p>
-                  <p class="office">Office Number: ${data.officeNumber}</p>
+                  <p class="id">ID: ${manager.id}</p>
+                  <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                  <p class="office">Office Number: ${manager.officeNumber}</p>
               </div>
           </div>
       </div>
@@ -62,7 +62,7 @@ function generateHTML(data) {
       pageArray = []; 
   
       for (let i = 0; i < data.length; i++) {
-          const employee = data[i];
+          const Employee = data[i];
           const role = employee.getRole(); 
   
   
